0d9cc602f9f66c346bf20ea8af9383f7ae66d8c4
Sang <tkddud1237@gmail.com>
2018-01-13 00:40:29 +0900

    2018.1.13

---
+++ src/AmpOpnd.java
@@ -0,0 +1,9 @@
+/**
+ * Created by wwf12 on 2018-01-13.
+ */
+public class AmpOpnd {
+    public static void main(String[] args) {
+        System.out.println("정수형 나머지 : " + 7%3);
+        System.out.println("실수형 나머지 : " + 7.2 % 2.0);
+    }
+}
