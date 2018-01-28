b4a7f8d59932c64db1c9a98a4609778ea4aaa19b
Sang <tkddud1237@gmail.com>
2018-01-28 23:49:11 +0900

    2018.1.28

---
+++ Chapter5/src/IFBasic.java
@@ -0,0 +1,20 @@
+/**
+ * Created by wwf12 on 2018-01-28.
+ */
+public class IFBasic {
+    public static void main(String[] args) {
+        if(true)
+        {
+            System.out.println("if & true");
+        }
+
+        if(false)
+        {
+            System.out.println("if~else & true");
+        }
+        else
+        {
+            System.out.println("if~else & false");
+        }
+    }
+}
