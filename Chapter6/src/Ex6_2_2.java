0b69465eb68dabe3cae0251a200920fc6045cbe8
Sang <tkddud1237@gmail.com>
2018-02-06 00:33:13 +0900

    2018.2.6

--- Chapter6/src/Ex6_2_2.java
+++ Chapter6/src/Ex6_2_2.java
@@ -4,7 +4,20 @@
 public class Ex6_2_2 {
     public static void main(String[] args) {
         for(int i=1;i<=100;i++) {
-            
+            if(isPrimeNumber(i))
+                System.out.println(i);
         }
+    }
+
+    public static boolean isPrimeNumber(int num) {
+        if(num == 1)
+            return false;
+
+        for(int i=2;i<num;i++) {
+            if(num%i == 0)
+                return false;
+        }
+
+        return true;
     }
 }
