121ad14481e8beacd3e142c92e80d8cdbd61b807
Sang <tkddud1237@gmail.com>
2018-02-13 23:44:13 +0900

    2018.2.13

--- src/Ex4_1_3.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by wwf12 on 2018-01-25.
- */
-public class Ex4_1_3 {
-    public static void main(String[] args) {
-        int result = ((25*5)+(36-4)-72)/5;
-        System.out.println("{(25*5)+(36-4)-72}/5 = " + result);
-    }
-}
